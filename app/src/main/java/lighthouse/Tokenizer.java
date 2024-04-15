dc926074a40b5f7a94152c672bc93bf5d6abab4d
nmarth2993 <nmarth2993@gmail.com>
2024-04-15 16:49:41 -0400

    Create gradle project

---
+++ app/src/main/java/lighthouse/Tokenizer.java
@@ -0,0 +1,73 @@
+package lighthouse;
+
+import java.io.*;
+
+public class Tokenizer {
+
+    public static void indexDirectory(String inputDir, String outputDir) {
+        /*
+         * Allow for only input/output parameter to be passed, with -1 as maxDocuments
+         * -> all documents parsed
+         */
+        indexDirectory(inputDir, outputDir, -1);
+    }
+
+    public static void indexDirectory(String inputDirPath, String outputDirPath, int maxDocuments) {
+        /*
+         * Perform tokenization across all files in input directory +
+         * Place resulting token files in the specified output directory
+         * 
+         * Will parse UP TO maxDocuments # of files.
+         */
+
+        // if directory does not exist, make it
+        File inputDir = new File(inputDirPath);
+        if (!inputDir.exists()) {
+            if (inputDir.mkdirs()) {
+                System.out.println("Input directory created as it did not exist prior.");
+            } else {
+                System.out.println("Input directory does not exist, and program failed to create it.");
+            }
+        }
+
+        // loop through all files in input directory
+        if (inputDir.exists() && inputDir.isDirectory()) {
+            // create array of pathnames to files in the directory
+            File[] files = inputDir.listFiles();
+            System.out.println(String.format("Found %d documents to index in input directory.", files.length));
+            if (files != null) {
+                // loop through each file in the array of all files in input directory
+                for (File file : files) {
+
+                }
+            }
+        } else {
+            System.out.println("Directory does not exist or is not a directory.");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String inputDir = "./files/";
+        String outputDir = "./files_out/";
+
+        if (args.length != 2) {
+            System.out.println("Please pass an input/output directory. Proceeding with default values.");
+        } else {
+            inputDir = args[0];
+            outputDir = args[1];
+
+            inputDir = inputDir.replace("\\", "/");
+            outputDir = outputDir.replace("\\", "/");
+
+            if (!inputDir.endsWith("/")) {
+                inputDir += "/";
+            }
+            if (!outputDir.endsWith("/")) {
+                outputDir += "/";
+            }
+
+            indexDirectory(inputDir, outputDir);
+        }
+    }
+}
---
+++ app/src/test/java/lighthouse/LibraryTest.java
@@ -0,0 +1,35 @@
+package lighthouse;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+public class LibraryTest {
+    @Test
+    void testJSoup() {
+        String html = "<html><head><title>First parse</title></head>"
+                + "<body><p>Parsed HTML into a doc.</p></body></html>";
+        Document doc = Jsoup.parse(html);
+    }
+
+    @Test
+    void testCSVParser() {
+        try {
+            StringReader stringReader = new StringReader("CSV, Parser\nRecord2, 2");
+            CSVParser parser = new CSVParser(stringReader, CSVFormat.DEFAULT);
+            parser.getRecords();
+            long parsedRecords = parser.getRecordNumber();
+            parser.close();
+        } catch (IOException e) {
+            assertTrue(false, "CSV Parser encountered IO error");
+        }
+    }
+}
