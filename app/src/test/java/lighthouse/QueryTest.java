9e9bb15bd7d8af832bbdc4ba597ccb3c8143dbc9
nmarth2993 <nmarth2993@gmail.com>
2024-05-01 07:05:05 -0400

    Add QueryEngine tests

---
+++ app/src/test/java/lighthouse/QueryTest.java
@@ -0,0 +1,34 @@
+package lighthouse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class QueryTest {
+
+    static QueryEngine engine;
+
+    @BeforeAll
+    static void createQueryEngine() {
+        engine = new QueryEngine("..\\tokens");
+    }
+
+    @Test
+    void orderingTest() {
+        var result = engine.query("test");
+        for (int i = 0; i < result.size() - 1; i++) {
+            assert result.get(i).getValue() >= result.get(i + 1).getValue();
+        }
+    }
+
+    @Test
+    void queryTest() {
+        var result = engine.query("spaghetti");
+        assertNotEquals(0, result.size());
+
+        var testResult = result.stream().filter(r -> r.getKey().equals("041.csv")).findAny();
+        assertEquals(true, testResult.isPresent());
+    }
+}
